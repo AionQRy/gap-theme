2dc690552995566e386aa78d4e1a32bf6fceb10d
AionQRy <anacuse15@hotmail.com>
2022-07-16 12:23:24 +0700

    update

--- src/components/RelatedPost/RelatedPost.js
+++ src/components/RelatedPost/RelatedPost.js
@@ -3,7 +3,9 @@ import { RelatedWrapper } from './RelatedPost.styles'
 import { useRelatedpostQuery } from '../../hook/useRelatedpostQuery'
 
 const RelatedPost = (currentPost) => {
-    console.log(useRelatedpostQuery);
+
+  const allWpPost = useRelatedpostQuery();
+    console.log(allWpPost);
   return (
     <RelatedWrapper>
         RelatedPost
