27ac885c18c9a64479a055b5b180f2f04963b7c5
AionQRy <anacuse15@hotmail.com>
2022-08-13 16:09:14 +0700

    up

--- src/components/Home/WhatAbout/WhatAbout.js
+++ src/components/Home/WhatAbout/WhatAbout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { AboutWrapper } from './WhatAbout.styles';
 import { Link } from 'gatsby';
 import { ArrowRight } from 'react-feather';
+import { GatsbyImage } from 'gatsby-plugin-image'
+
 const WhatAbout = () => {
   return (
     <AboutWrapper>
@@ -10,10 +12,16 @@ const WhatAbout = () => {
                 <div className="TitleAbout">
                 
                 {/* {topTitle.length > 0 ? ( */}
+                <div className="head">
                     <h2>WordPress คืออะไร</h2>
+                </div>
+                    
                 {/* ):null } */}
                 {/* {detail.length > 0 ? ( */}
+                <div className="head">
                     <p>ระบบ WordPress คือระบบที่ช่วยในการสร้างเว็บไซต์จากเรื่องยากให้เป็นเรื่องง่าย เพราะว่าผู้ใช้งาน สามารถจัดการและสร้างเนื้อหาเว็บไซต์ได้ด้วยตนเองโดยไม่ต้องเขียนโค้ดซึ่ง ระบบแบบนี้ เรียกว่า (Content Management System : CMS ) และในปัจจุบัน กว่า 43% ของเว็บไซต์ทั่วโลก ทำเว็บไซต์ด้วย WordPress</p>
+                </div>
+                    
                 {/* ):null } */}
                     <div className="box-readmore">
                         <Link className="BtnReadmore">ดูรายละเอียด<ArrowRight/></Link>
